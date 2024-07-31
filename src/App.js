4c1d6caab73151b63f482672fe49f757ac16ad4c
Hongwei Ruan <hwruan@umich.edu>
2024-07-31 17:45:26 -0400

    input newplace

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NewPlace from "./places/pages/NewPlace";
 import Users from "./user/pages/Users";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/components/UpdatePlace";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Users />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="/:uid/places" element={<UserPlaces />} />
+          <Route path="/places/:placeId" element={<UpdatePlace />}/>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
